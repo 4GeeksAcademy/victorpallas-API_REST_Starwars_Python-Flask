f4ade2180e9af51f7043978b9fcbc5d9d251d86c
victorpallas1 <victorpallas@seguridadea1.com>
2025-04-16 18:49:33 +0000

    1

--- src/models.py
+++ src/models.py
@@ -1,19 +1,48 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+class User(db.Model):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(
+        String(60), unique=False, nullable=False)
+    firstname: Mapped[str] = mapped_column(
+        String(60), unique=False, nullable=False)
+    lastname: Mapped[str] = mapped_column(
+        String(60), unique=False, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class People(db.Model):
+    __tablename__ = "people"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_from_id: Mapped[int] = mapped_column(
+        ForeignKey('user.id'), nullable=False)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+
+
+class Planet(db.Model):
+    __tablename__ = "planet"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_from_id: Mapped[int] = mapped_column(
+        ForeignKey('user.id'), nullable=False)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
