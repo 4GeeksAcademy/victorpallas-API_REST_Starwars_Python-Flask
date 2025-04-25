d65be96027c14de6f3ace83a389aedca594a2042
victorpallas1 <victorpallas@seguridadea1.com>
2025-04-25 19:15:19 +0000

    intentando enterder

--- src/models.py
+++ src/models.py
@@ -1,22 +1,25 @@
+import enum
+from typing import List
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
+class Favorites_Types(enum.Enum):
+    planet = 1
+    people = 2
+    vehicle = 3
+    film = 4
 
 class User(db.Model):
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(
-        String(60), unique=False, nullable=False)
-    firstname: Mapped[str] = mapped_column(
-        String(60), unique=False, nullable=False)
-    lastname: Mapped[str] = mapped_column(
-        String(60), unique=False, nullable=False)
-    email: Mapped[str] = mapped_column(
-        String(120), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    firstname: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    lastname: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
     def serialize(self):
@@ -26,23 +29,34 @@ class User(db.Model):
             "firstname": self.firstname,
             "lastname": self.lastname,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
-
 
 class People(db.Model):
     __tablename__ = "people"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_from_id: Mapped[int] = mapped_column(
-        ForeignKey('user.id'), nullable=False)
+    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
     name: Mapped[str] = mapped_column(String(120), nullable=False)
 
+    favorites: Mapped[List["Favorites"]] = relationship("Favorites", back_populates="people")
 
 class Planet(db.Model):
     __tablename__ = "planet"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_from_id: Mapped[int] = mapped_column(
-        ForeignKey('user.id'), nullable=False)
+    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
     name: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    favorites: Mapped[List["Favorites"]] = relationship("Favorites", back_populates="planet")
+
+class Favorites(db.Model):
+    __tablename__ = "favorites"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[Favorites_Types] = mapped_column(Enum(Favorites_Types))
+    
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planet.id"), nullable=True)
+    people_id: Mapped[int] = mapped_column(ForeignKey("people.id"), nullable=True)
+
+    planet: Mapped["Planet"] = relationship("Planet", back_populates="favorites")
+    people: Mapped["People"] = relationship("People", back_populates="favorites")
