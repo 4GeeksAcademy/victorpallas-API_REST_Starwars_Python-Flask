2587090212e6c503a2d2f0728c4f7bd43effc1de
victorpallas1 <victorpallas@seguridadea1.com>
2025-04-30 19:17:28 +0000

    dia_30

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Favorites, Favorites_Types, People, Planet
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,23 +28,47 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_all_users():
+    users = User.query.all()
+    return jsonify([user.serialize()for user in users]),200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user_id(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"msg": "El usuario no existe"}),404
+    return jsonify(user.serialize()),200
 
-    return jsonify(response_body), 200
+@app.route('/planet', methods=['GET'])
+def get_all_planets():
+    planets = Planet.query.all()
+    return jsonify([planet.serialize()for planet in planets]),200
+    
+
+
+@app.route('/people', methods=['GET'])
+def get_all_peoples():
+    peoples = People.query.all()
+    return jsonify([unaPersona.serialize()for unaPersona in peoples ]),200
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -6,21 +6,28 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
+
 class Favorites_Types(enum.Enum):
     planet = 1
     people = 2
     vehicle = 3
     film = 4
 
+
 class User(db.Model):
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
-    firstname: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
-    lastname: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(
+        String(60), unique=False, nullable=False)
+    firstname: Mapped[str] = mapped_column(
+        String(60), unique=False, nullable=False)
+    lastname: Mapped[str] = mapped_column(
+        String(60), unique=False, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    favorites: Mapped[list["Favorites"]] = relationship(back_populates="user")
 
     def serialize(self):
         return {
@@ -31,32 +38,70 @@ class User(db.Model):
             "email": self.email,
         }
 
+
 class People(db.Model):
     __tablename__ = "people"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    user_from_id: Mapped[int] = mapped_column(
+        ForeignKey('user.id'), nullable=True)
     name: Mapped[str] = mapped_column(String(120), nullable=False)
 
-    favorites: Mapped[List["Favorites"]] = relationship("Favorites", back_populates="people")
+    favorites: Mapped[List["Favorites"]] = relationship(
+        "Favorites", back_populates="people")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "favoritecount": len(self.favorites)
+
+        }
+
 
 class Planet(db.Model):
     __tablename__ = "planet"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    #user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
     name: Mapped[str] = mapped_column(String(120), nullable=False)
 
-    favorites: Mapped[List["Favorites"]] = relationship("Favorites", back_populates="planet")
+    favorites: Mapped[List["Favorites"]] = relationship(
+        "Favorites", back_populates="planet")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            #"username": self.username,
+            "name": self.name,
+            "favoritecount": len(self.favorites)
+
+        }
+
 
 class Favorites(db.Model):
     __tablename__ = "favorites"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     type: Mapped[Favorites_Types] = mapped_column(Enum(Favorites_Types))
-    
-    planet_id: Mapped[int] = mapped_column(ForeignKey("planet.id"), nullable=True)
-    people_id: Mapped[int] = mapped_column(ForeignKey("people.id"), nullable=True)
 
-    planet: Mapped["Planet"] = relationship("Planet", back_populates="favorites")
-    people: Mapped["People"] = relationship("People", back_populates="favorites")
+    planet_id: Mapped[int] = mapped_column(
+        ForeignKey("planet.id"), nullable=True)
+    people_id: Mapped[int] = mapped_column(
+        ForeignKey("people.id"), nullable=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=True)
+
+    planet: Mapped["Planet"] = relationship(
+        "Planet", back_populates="favorites")
+    people: Mapped["People"] = relationship(
+        "People", back_populates="favorites")
+    user: Mapped[User] = relationship(back_populates="favorites")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.type.name,
+            "user_id": self.user.id,
+            "people_id": self.people.id,
+            "planet_id": self.planet.id
+        }
